82ebbb4c974bdcaf76111cbb366be9cc9f17b0b3
Masoodi97 <96894647+Masoodi97@users.noreply.github.com>
2022-12-11 13:46:28 -0600

    updated index code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
 
-router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+const postroutes = require('./postRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/user', userRoutes);
+router.use('/post', postroutes);
 
 module.exports = router;
